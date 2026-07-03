43b3a514b48a9a4e39a162dfa847e58908b569b2
MurilloBastos7 <murillobastos727@gmail.com>
2026-07-03 20:24:54 -0300

    Pandas/Numpy

---
+++ app.py
@@ -0,0 +1,11 @@
+# comando para exibir informações na tela
+print("boa noite")
+
+idade = 30
+idade_dois = "30"
+idade_tres = 30.5
+
+# type retorna o tipo da variavel
+print(type(idade))
+print(type(idade_dois))
+print(type(idade_tres))
